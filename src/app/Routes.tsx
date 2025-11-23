37a1444b161a6da36a9139cdbd0e37eaeb88e3ca
developerjhp <developerjhp@gmail.com>
2025-11-23 22:42:25 +0900

    chore(app): Routes 경로 업데이트
      - SavingsCalculatorPage 경로 변경에 따른 import 수정

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
-import { SavingsCalculatorPage } from '@pages/SavingsCalculatorPage';
+import { SavingsCalculatorPage } from '@pages/SavingsCalculatorPage/SavingsCalculatorPage';
 
 const router = createBrowserRouter([
   {
